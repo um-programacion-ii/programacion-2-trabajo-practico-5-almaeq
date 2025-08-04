8cd889121cd4f922a8a9cc7b444fa3169a203756
almaeq <alma.quinterosquiroga@gmail.com>
2025-08-04 11:20:40 -0300

    commit inicial

---
+++ src/main/java/com/example/sistemaGestionEmpleados/SistemaGestionEmpleadosApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemaGestionEmpleados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionEmpleadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionEmpleadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistemaGestionEmpleados/SistemaGestionEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemaGestionEmpleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionEmpleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
