a7daaeb1115e8faddaa5ee9afdb6eb823cbd930f
almaeq <alma.quinterosquiroga@gmail.com>
2025-08-05 11:00:18 -0300

    add empleado service

---
+++ src/main/java/com/example/sistemaGestionEmpleados/services/EmpleadoService.java
@@ -0,0 +1,17 @@
+package com.example.sistemaGestionEmpleados.services;
+
+import com.example.sistemaGestionEmpleados.models.Empleado;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface EmpleadoService {
+    Empleado guardar(Empleado empleado);
+    Empleado buscarPorId(Long id);
+    List<Empleado> buscarPorDepartamento(String nombreDepartamento);
+    List<Empleado> buscarPorRangoSalario(BigDecimal salarioMin, BigDecimal salarioMax);
+    BigDecimal obtenerSalarioPromedioPorDepartamento(Long departamentoId);
+    List<Empleado> obtenerTodos();
+    Empleado actualizar(Long id, Empleado empleado);
+    void eliminar(Long id);
+}
